a2e8016b60cffe5bffa05abb576ad68bf1969365
yantene <contact@yantene.net>
2021-03-20 22:01:40 +0900

    Add .github/workflows/main.yaml

--- src/hello.ts
+++ src/hello.ts
@@ -1,8 +1,17 @@
 /**
- * 引数 name に対するあいさつの文字列を返します
+ * 引数 name に対するこんにちはのあいさつの文字列を返します
  * @param name あいさつの対象者の名前
  * @return あいさつの文字列
  */
 export default function hello(name: string): string {
   return `Hello, ${name}!!`
+}
+
+/**
+ * 引数 name に対するさようならのあいさつの文字列を返します
+ * @param name あいさつの対象者の名前
+ * @return あいさつの文字列
+ */
+export function goodbye(name: string): string {
+  return `Good bye, ${name}!!`
 }
